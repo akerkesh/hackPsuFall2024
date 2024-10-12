ab801213f66b57434264bad7fc7262e721e5e2f2
Ny1ka <nwachira07@gmail.com>
2024-10-12 18:17:58 -0400

    trying to set up database to run website locally on computers

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 # app/__init__.py
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_wtf.csrf import CSRFProtect
 from config import Config
 
 mongo = None
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    csrf = CSRFProtect(app)  # Initialize CSRF protection
 
     global mongo
     mongo = PyMongo(app)  # Initialize MongoDB connection
