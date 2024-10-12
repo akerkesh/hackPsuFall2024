88e5cc41276459db583f5eb5cf9d4dd3576618d7
Ny1ka <nwachira07@gmail.com>
2024-10-12 15:31:43 -0400

    updated uri key

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     # MongoDB URI (replace with your MongoDB Atlas or local URI)
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://username:password@cluster.mongodb.net/quackademic')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://nwachira07:<ZPAifCU4tv675GgC>@hackpsu2024.kw4zu.mongodb.net/')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'P\xc2\xf0\xecW\xb0\xa4\x19\xbccZ7\xafT\xb0\xe7m\xd1ZmR\x19\xff\\')
